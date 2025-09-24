582278a57fd8451103402148451f03316974017f
dudu-soliveira <eduardosoliveira2003@gmail.com>
2025-09-24 10:19:13 -0300

    feat: daily 24/09/25

---
+++ medium/0166.fraction-to-recurring-decimal.ts
@@ -0,0 +1,44 @@
+// @leet start
+function fractionToDecimal(numerator: number, denominator: number): string {
+  if (numerator === 0) return "0";
+
+  let negative = false;
+  if (numerator < 0) {
+    negative = !negative;
+    numerator *= -1;
+  }
+  if (denominator < 0) {
+    negative = !negative;
+    denominator *= -1;
+  }
+
+  const int = negative
+    ? `-${Math.floor(numerator / denominator)}`
+    : String(Math.floor(numerator / denominator));
+  numerator %= denominator;
+
+  if (numerator === 0) return String(int);
+
+  let results: number[] = [];
+  let operationMap: Map<string, number> = new Map();
+
+  for (let i = 0; i < 10000; i++) {
+    numerator *= 10;
+    const div = Math.floor(numerator / denominator);
+    numerator %= denominator;
+
+    const key = `${div}-${numerator}`;
+    const repeatStart = operationMap.get(key);
+
+    if (repeatStart !== undefined)
+      return `${int}.${results.slice(0, repeatStart).join("")}(${results.slice(repeatStart).join("")})`;
+
+    results.push(div);
+    operationMap.set(key, i);
+
+    if (numerator === 0) break;
+  }
+
+  return `${int}.${results.join("")}`;
+}
+// @leet end
