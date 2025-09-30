f8c21688d366b68ef5231a9915e00eb98b21756e
dudu-soliveira <eduardosoliveira2003@gmail.com>
2025-09-29 23:27:23 -0300

    feat: update 2221 to have O(1) space complexity

--- medium/2221.find-triangular-sum-of-an-array.ts
+++ medium/2221.find-triangular-sum-of-an-array.ts
@@ -3,20 +3,19 @@
 // @leet start
 function triangularSum(nums: number[]): number {
   const n = nums.length;
-  let multArr: bigint[] = Array(n).fill(1n);
-  let ans = 0n;
+
+  if (n === 1) return nums[0];
+
+  let mult = 1n;
+  let ans = BigInt(nums[0] + nums[n - 1]) % 10n;
 
   for (let i = 1; i < Math.ceil(n / 2); i++) {
-    const mult = (multArr[i - 1] * BigInt(n - i)) / BigInt(i);
-    multArr[i] = mult;
-    multArr[n - i - 1] = mult;
-  }
-
-  for (let i = 0; i < n; i++) {
-    ans = (ans + BigInt(nums[i]) * multArr[i]) % 10n;
+    mult = (mult * BigInt(n - i)) / BigInt(i);
+    ans = (ans + ((BigInt(nums[i]) * mult) % 10n)) % 10n;
+    if (i !== n - i - 1)
+      ans = (ans + ((BigInt(nums[n - i - 1]) * mult) % 10n)) % 10n;
   }
 
   return Number(ans);
 }
 // @leet end
-
